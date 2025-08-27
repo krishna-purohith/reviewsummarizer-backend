c6ba7e3cd62ab46f4a5b419a17544e0bde9f6893
Krishna Purohith <purohithkrishna.1@gmail.com>
2025-08-27 23:02:32 +0530

    add cors

--- index.ts
+++ index.ts
@@ -1,10 +1,20 @@
 import dotenv from 'dotenv';
 import express from 'express';
+import cors from 'cors';
 import router from './routes';
 
 dotenv.config();
 
 const app = express();
+
+const allowedOrigins = process.env.CORS_ORIGIN?.split(',') || ['*'];
+
+app.use(
+   cors({
+      origin: allowedOrigins,
+      credentials: true,
+   })
+);
 app.use(express.json());
 app.use(router);
 
